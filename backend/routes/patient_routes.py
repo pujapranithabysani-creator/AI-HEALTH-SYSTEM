7fba30dc34f564acb4a627e32a4dafb2b91064d5
Your Name <you@example.com>
2026-06-01 00:00:01 +0530

    Fix file structure and absolute imports for vercel

--- backend/routes/patient_routes.py
+++ backend/routes/patient_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from models.patient_model import db, Patient
+from backend.models.patient_model import db, Patient
 
 patient_bp = Blueprint('patient_bp', __name__)
 
